6ef0c89d798177d04f34d2c121c4eb6263f9abb6
leanfiadone <leanfiadone@gmail.com>
2024-01-27 02:39:45 -0300

    prueba

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>Hola, soy Leandro Fiadone</p>
+        <p>
+          Desde marzo de 2023, he sido un Desarrollador Full-Stack con un fuerte
+          enfoque en Front-End en Pl4nner.com, aprovechando mis +800 horas de
+          estudio dedicado en SoyHenry.com. Aunque me especializo en React,
+          Next.js, Node.js y MongoDB para el desarrollo del front-end, también
+          tengo un profundo interés en el desarrollo de inteligencia artificial
+          y blockchain. Esta fascinación impulsa mi exploración y aprendizaje
+          continuo en estos campos. Mi base en principios de diseño y producción
+          audiovisual complementa mi pasión por crear soluciones intuitivas de
+          front-end. ¡Estoy comprometido a evolucionar mis habilidades,
+          mantenerme actualizado con las tecnologías emergentes y contribuir con
+          ideas innovadoras a cada proyecto! 💻📚🌍
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
